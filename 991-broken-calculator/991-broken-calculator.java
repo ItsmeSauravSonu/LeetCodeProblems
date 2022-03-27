ae1fb46f99a2d9dc14faba65fd0453a071674ac1
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-03-28 00:02:52 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (56.79%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int brokenCalc(int st, int ta) {
+        int mn = 0;
+          while( st <ta){
+                  if( ta%2!=0) mn+=2;
+                  else mn++;
+                  ta=(ta+1)/2;
+          }
+            return  mn+( st-ta);
+    }
+}
