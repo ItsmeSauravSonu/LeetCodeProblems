80fec1e2adcb1fa2147e5ffcaa62a83d7ef3629e
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-06-06 19:13:17 +0530

    Time: 10 ms (32.20%), Space: 44.5 MB (63.76%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-       int m = word1.length();
-       int n = word2.length();
-        
-        int [][] t = new int[m+1][n+1];
-        
-        for(int i=1;i<=m;i++)
+    public int minDistance(String str1, String str2) {
+   int [][] dp = new int[str2.length()+1][str1.length()+1];
+   for(int i=0;i<str2.length()+1;i++)
+     {
+       for(int j=0;j<str1.length()+1;j++)
+         {
+           dp[i][j]=j;
+           
+         }
+       dp[i][0]=i;
+     }
+  
+  for(int i=1;i<str2.length()+1;i++)
+    {
+      for(int j=1;j<str1.length()+1;j++)
         {
-            t[i][0]=i;
+          if(str2.charAt(i-1)==str1.charAt(j-1)){
+            dp[i][j]=dp[i-1][j-1];
+          }else{
+            dp[i][j]= 1+ Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+          }
         }
-        for(int j=1;j<=n;j++)
-        {
-            t[0][j]=j;
-        }
-        int cost;
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    cost = 0;
-                }else{
-                    cost = 1;
-                }
-                
-                t[i][j]=Math.min(Math.min(t[i-1][j]+1,t[i][j-1]+1),t[i-1][j-1]+cost);
-            }
-        }
-        return t[m][n];
+    }
+   return dp[str2.length()][str1.length()];
     }
 }
