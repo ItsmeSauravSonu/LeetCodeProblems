26aa655804916807382c8425e24b241c45f32c68
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-06-13 22:57:42 +0530

    Time: 4 ms (69.63%), Space: 57.8 MB (24.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,33 @@
-public class Solution {
-
-private int n;
-private int m;
-
-public int numIslands(char[][] grid) {
-    int count = 0;
-    n = grid.length;
-    if (n == 0) return 0;
-    m = grid[0].length;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++)
-            if (grid[i][j] == '1') {
-                DFSMarking(grid, i, j);
-                ++count;
-            }
-    }    
+class Solution {
+    public int numIslands(char[][] grid) {
+      if(grid == null || grid.length==0)
+          return 0;
+      boolean [][] visited = new boolean[grid.length][grid[0].length];
+      int count = 0;
+      for(int i=0;i<grid.length;i++)
+      {
+          for(int j=0;j<grid[0].length;j++)
+          {
+              if(grid[i][j]=='1'  && visited[i][j]==false)
+              {
+                  dfs(grid, visited, i, j);
+                  count++;
+              }
+          }
+      }
     return count;
-}
-
-private void DFSMarking(char[][] grid, int i, int j) {
-    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-    grid[i][j] = '0';
-    DFSMarking(grid, i + 1, j);
-    DFSMarking(grid, i - 1, j);
-    DFSMarking(grid, i, j + 1);
-    DFSMarking(grid, i, j - 1);
-}
+    }
+    
+    public void dfs(char [][] grid, boolean [][] visited, int i, int j)
+    {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || visited[i][j]==true || grid[i][j]=='0')
+        {
+            return;
+        }
+        visited[i][j]=true;
+        dfs(grid, visited, i,j+1);
+        dfs(grid, visited,i+1,j);
+        dfs(grid,visited,i,j-1);
+        dfs(grid,visited,i-1,j);
+    }
 }
