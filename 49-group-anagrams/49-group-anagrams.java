4fa10f2f9983ac12a2f959014336254771f98f3e
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-06-19 22:15:02 +0530

    Time: 7 ms (95.25%), Space: 45.4 MB (92.83%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+      Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
