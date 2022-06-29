7dbe4ea5bb206133fa84e583ec95231eee2bde68
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-06-29 18:42:17 +0530

    Time: 2 ms (61.94%), Space: 50.9 MB (36.11%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int x: nums)
+        {
+            res^=x;
+        }
+        return res;
+    }
+}
