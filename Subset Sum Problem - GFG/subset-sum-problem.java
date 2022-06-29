4f4fc3b5e3ea4cb9381e19b8f052b2e040b9d858
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-06-29 17:05:30 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+
+    static Boolean isSubsetSum(int n, int set[], int sum){
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++)
+            subset[0][i] = true;
+
+        // If sum is not 0 and set is empty,
+        // then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[i][0] = false;
+
+        // Fill the subset table in bottom
+        // up manner
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1])
+                    subset[i][j] = subset[i][j]
+                                   || subset[i - set[j - 1]][j - 1];
+            }
+        }
+
+        /* // uncomment this code to print table
+        for (int i = 0; i <= sum; i++)
+        {
+        for (int j = 0; j <= n; j++)
+            System.out.println (subset[i][j]);
+        } */
+
+        return subset[sum][n];    }
+}
