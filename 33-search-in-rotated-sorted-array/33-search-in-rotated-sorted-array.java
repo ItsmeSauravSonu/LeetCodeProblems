1e33e8d5cb82724944a0b1a65e5e1be0ac689503
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-04 12:19:31 +0530

    Time: 1 ms (72.69%), Space: 42.4 MB (67.50%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,33 +1,40 @@
 class Solution {
-    public int search(int[] array, int target) {
-       int left = 0;
-  int right = array.length-1;
-
-    while(left<=right)
-      {
-        int mid = left + (right-left)/2;
-        int potentialMatch = array[mid];
-        int leftNum = array[left];
-        int rightNum = array[right];
-
-        if(target == potentialMatch){
-          return mid;
-        }else if(leftNum <= potentialMatch)
-        {
-          if(target<potentialMatch && target >= leftNum)
-          {
-            right = mid-1;
-          }else{
-            left = mid+1;
-          }
-        }else{
-          if(target>potentialMatch && target<=rightNum){
-            left = mid+1;
-          }else{
-            right = mid-1;
-          }
+    public int search(int[] nums, int target) {
+        int st=0,mid=0,next=0,prev=0,n=nums.length,end=n-1,x=n-1;
+        while(st<=end){
+            mid=st+(end-st)/2;
+            next=(mid+1)%n;
+            prev=(mid+n-1)%n;
+            if(nums[mid]<nums[next]&&nums[mid]<nums[prev])
+            {x=mid;
+             break;
+            }
+            else if(nums[0]<=nums[mid]){
+                st=mid+1;
+            }
+            else if(nums[mid]<=nums[nums.length-1])
+                end=mid-1;
         }
-      }
-    return -1;
+         st=0;end=x-1;
+        while(st<=end){
+            mid=st+(end-st)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else st=mid+1;
+        }
+        st=x;end=n-1;
+        while(st<=end){
+            mid=st+(end-st)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else st=mid+1;
+        }
+        return -1;
     }
 }
