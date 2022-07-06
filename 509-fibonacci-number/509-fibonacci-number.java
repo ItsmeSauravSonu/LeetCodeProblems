d3582392b5d5f2db81b50ca8ecf5690eed5da9c7
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-06 07:34:45 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (22.59%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1)
+        {
+            return n;
+        }
+        int [] f = new int[n+1];
+        f[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            f[i]=f[i-1]+f[i-2];
+        }
+        return f[n];
+    }
+}
