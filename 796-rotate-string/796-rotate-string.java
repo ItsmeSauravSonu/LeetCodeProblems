1f9497b4ab7b83e27383fa5759d0f4db57fa8bd0
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-11 17:50:59 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (82.98%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean rotateString(String str1, String str2) {
+        return (str1.length() == str2.length()) &&
+               ((str1 + str1).indexOf(str2) != -1);
+    }
+}
