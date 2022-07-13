b817ce33537ed1b64700a4e161db8378bddb9b14
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-13 10:53:11 +0530

    Time: 1 ms (20.84%), Space: 41.8 MB (30.36%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int f(int idx, int []nums, int []dp)
-    {
-        if(idx==0) return nums[0];
-        if(idx<0) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int choriKiya = nums[idx]+f(idx-2,nums,dp);
-        int choriNiKiya = 0+f(idx-1,nums,dp);
-        return dp[idx]=Math.max(choriKiya, choriNiKiya);
-    }
+    
     public int rob(int[] nums) {
       int n = nums.length;
       
       int [] dp = new int[n];
-      Arrays.fill(dp,-1);
-      return f(n-1, nums,dp);
+      dp[0]=nums[0];
+      for(int i=1;i<n;i++)
+      {
+          int pick = nums[i];
+          if(i>1)
+          {
+             pick +=dp[i-2]; 
+          }
+          int npick = 0+dp[i-1];
+          dp[i]=Math.max(pick,npick);
+      }
+        return dp[n-1];
     }
 }
