4fa3de4a52148dd2b3f5ecaa93432fb6ce3755be
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-15 10:34:16 +0530

    Time: 1 ms (99.98%), Space: 46.1 MB (40.98%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,31 +1,29 @@
 class Solution {
+    public int minSumPathUtil(int i, int j, int [][] matrix, int [][]dp)
+    {
+       if(i==0 && j == 0){
+           return matrix[0][0];
+       }
+    
+  if(i<0 || j<0){
+       return (int)Math.pow(10,9);
+  }
+   
+  if(dp[i][j]!=-1) return dp[i][j];
+    
+  int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
+  int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
+  
+  return dp[i][j] = Math.min(up,left);
+    }
     public int minPathSum(int[][] grid) {
-
-        int m = grid.length; int n = grid[0].length;
-        
-        
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        
-        
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = grid[i][0] + dp[i-1][0];
+        int n = grid.length;
+        int m = grid[0].length;
+        int [][] dp = new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(dp[i],-1);
         }
-        
-        
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = grid[0][i] + dp[0][i-1];
-        }
-        
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-               
-                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        
-        
-        return dp[m-1][n-1];
+        return minSumPathUtil(n-1,m-1,grid,dp);
     }
 }
