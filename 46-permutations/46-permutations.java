1238e90f03698db91f841c78b6fc68589c118c32
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-07-25 18:58:16 +0530

    Time: 4 ms (24.99%), Space: 44.7 MB (53.79%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,22 +1,22 @@
 class Solution {
-    List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-
-  
-   backtrack( new ArrayList<>(), nums);
-   return list;
-}
-
-private void backtrack( List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-   } else{
-      for(int i = 0; i < nums.length; i++){ 
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack( tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   }
-}
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(ans, new ArrayList<>(), nums);
+        return ans;
+    }
+    public void backtrack(List<List<Integer>> ans, List<Integer> temp, int []nums )
+    {
+        if(temp.size()==nums.length)
+        {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            backtrack(ans, temp, nums);
+            temp.remove(temp.size()-1);
+        }
+    }
 }
