68920125c32ce6b81a636534764aafe79ed78b16
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-04 12:42:15 +0530

    Time: 1 ms (96.41%), Space: 43.2 MB (12.09%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -18,26 +18,26 @@ class Solution {
      List<List<Integer>> ans = new ArrayList<>();
      if(root==null) return ans;
      int level = 0;
+    
      Queue<TreeNode> queue = new LinkedList<>();
      queue.offer(root);
-     while(!queue.isEmpty())
-     {
-         level = level+1;
-         int size = queue.size();
-         List<Integer> temp = new ArrayList<>();
-         for(int i=0;i<size;i++)
-         {
-             TreeNode node = queue.poll();
-             temp.add(node.val);
-             if(node.left!=null) queue.offer(node.left);
-             if(node.right!=null) queue.offer(node.right);
-         }
-         if(level%2==0)
-         {
-             Collections.reverse(temp);
-         }
-         ans.add(temp);
-     }
+    while(!queue.isEmpty())
+    {
+       level = level +1;
+       List<Integer> temp = new ArrayList<>();
+       int size = queue.size();
+       for(int i=0;i<size;i++){
+           TreeNode node = queue.poll();
+           temp.add(node.val);
+           if(node.left!=null) queue.offer(node.left);
+           if(node.right!=null) queue.offer(node.right);
+       }
+       if(level%2==0)
+       {
+           Collections.reverse(temp);
+       }
+        ans.add(temp);
+    }
         return ans;
     }
 }
