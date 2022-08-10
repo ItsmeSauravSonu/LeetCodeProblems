9f3fa997985bd176b7c326997bda0b3fd616d6a5
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-10 23:53:00 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (92.60%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,57 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+    
+    int [] ans = new int[2];
         
-        int [] ans = new int[2];
-         int first = left_bound(nums,target);
-         int second =right_bound(nums,target);
+    int first = left_bound(nums, target);
+    int second= right_bound(nums,target);
         
-        ans[0]=first;
-        ans[1]=second;
+    ans[0]=first;
+    ans[1]=second;
         
+    return ans;
+}
+    
+    public int left_bound(int [] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]==target){
+                ans = mid;
+                end = mid-1;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
         return ans;
     }
-  public int left_bound(int[] nums, int target) {
-    int left = 0, right = nums.length - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] < target) {
-            left = mid + 1;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else if (nums[mid] == target) {
-            // Don't return! Lock left border
-            right = mid - 1;
+    
+    public int right_bound(int [] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]==target){
+                ans = mid;
+                start = mid+1;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
+        return ans;
     }
-    // Check whether left border out of bounds lastly
-    if (left >= nums.length || nums[left] != target)
-        return -1;
-    return left;
-}
-
-
-public int right_bound(int[] nums, int target) {
-    int left = 0, right = nums.length - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] < target) {
-            left = mid + 1;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else if (nums[mid] == target) {
-            // Don't return! Lock right border
-            left = mid + 1;
-        }
-    }
-    // Check whether right border out of bounds lastly
-    if (right < 0 || nums[right] != target)
-        return -1;
-    return right;
-}
     
 }
