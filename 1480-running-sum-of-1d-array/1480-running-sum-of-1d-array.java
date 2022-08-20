294811e273ae267f12c90658ca69532a898f5533
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-20 22:21:36 +0530

    Time: 1 ms (20.15%), Space: 43.9 MB (9.03%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i=1;i<nums.length;i++) nums[i]+=nums[i-1]; //prefix sum calc
-        return nums;
-
+      int n = nums.length;
+      int [] ans = new int[n];
+      ans[0]=nums[0];
+      int sum = nums[0];
+      for(int  i=1;i<n;i++){
+          sum+=nums[i];
+          ans[i]=sum;
+      }
+      return ans;
     }
 }
