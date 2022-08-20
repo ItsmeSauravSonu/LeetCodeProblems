fed3e56d53bf58058e1ec63070a6407d8aec9c62
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-21 00:21:37 +0530

    Time: 118 ms (34.15%), Space: 91.9 MB (37.95%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -6,23 +6,26 @@ class Solution {
     // 7 - 1    
     
     public int minSetSize(int[] arr) {
-        int n = arr.length;
-        int half = n/2;
-        int count = 0;
-        //making the map count the occurences of the number
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        int l = arr.length;
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int x: arr) hs.put(x,hs.getOrDefault(x,0)+1);
+        int dp[]=new int[hs.size()];
+        int i=0;
+        for(Map.Entry e: hs.entrySet()){
+            int v=(int)e.getValue();
+            dp[i]=v;
+            i++;
         }
-        List<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list, Collections.reverseOrder());
-        for(int x: list){
-            if(n<=half){
-                return count;
-            }
-            n=n-x;
-            count++;
+        Arrays.sort(dp);
+        int t=l/2;
+        int c=0;
+        i=dp.length-1;
+        while(i>=0 &&  t>0){
+            t-=dp[i];
+            i--;
+            c++;
+            
         }
-        return count;
+        return c;
     }
 }
