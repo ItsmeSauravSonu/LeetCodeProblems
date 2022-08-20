d35b874369d17e4b4e0999ce30c3c70bac6a6f3b
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-21 00:33:01 +0530

    Time: 38 ms (89.62%), Space: 51.1 MB (95.54%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -6,25 +6,53 @@ class Solution {
     // 7 - 1    
     
     public int minSetSize(int[] arr) {
+//         int l = arr.length;
+//         HashMap<Integer,Integer> hs=new HashMap<>();
+//         for(int x: arr) hs.put(x,hs.getOrDefault(x,0)+1);
+//         int dp[]=new int[hs.size()];
+//         int i=0;
+//         for(Map.Entry e: hs.entrySet()){
+//             int v=(int)e.getValue();
+//             dp[i]=v;
+//             i++;
+//         }
+//         Arrays.sort(dp);
+//         int t=l/2;
+//         int c=0;
+//         i=dp.length-1;
+//         while(i>=0 &&  t>0){
+//             t-=dp[i];
+//             i--;
+//             c++;
+            
+//         }
+//         return c;
+        
         int l = arr.length;
         HashMap<Integer,Integer> hs=new HashMap<>();
         for(int x: arr) hs.put(x,hs.getOrDefault(x,0)+1);
-        int dp[]=new int[hs.size()];
         int i=0;
+        int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
         for(Map.Entry e: hs.entrySet()){
             int v=(int)e.getValue();
-            dp[i]=v;
-            i++;
+            min=Math.min(min,v);
+            max=Math.max(max,v); 
         }
-        Arrays.sort(dp);
-        int t=l/2;
+        int dp[]=new int[max-min+1];
+        for(Map.Entry e: hs.entrySet()){
+            int v=(int)e.getValue();
+            dp[v-min]+=1;
+        }
         int c=0;
-        i=dp.length-1;
-        while(i>=0 &&  t>0){
-            t-=dp[i];
-            i--;
-            c++;
-            
+        int t=l/2;
+        for(i=dp.length-1;i>=0;i--){
+            if(dp[i]>0){
+                while(t>0 && dp[i]>0){
+                    t-=(i+min);
+                    dp[i]-=1;
+                    c++;
+                }
+            }
         }
         return c;
     }
