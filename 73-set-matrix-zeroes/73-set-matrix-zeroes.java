373029662f99e149f66fd9fea2c0b755f2b23e2d
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-26 12:27:36 +0530

    Time: 2 ms (47.20%), Space: 54.3 MB (30.06%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,27 +1,34 @@
 class Solution {
-  public void setZeroes(int[][] matrix) {
-    int R = matrix.length;
-    int C = matrix[0].length;
-    Set<Integer> rows = new HashSet<Integer>();
-    Set<Integer> cols = new HashSet<Integer>();
-
-    // Essentially, we mark the rows and columns that are to be made zero
-    for (int i = 0; i < R; i++) {
-      for (int j = 0; j < C; j++) {
-        if (matrix[i][j] == 0) {
-          rows.add(i);
-          cols.add(j);
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        ArrayList<Integer> r1 = new ArrayList<>();
+        ArrayList<Integer> c1 = new ArrayList<>();
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    r1.add(i);
+                    c1.add(j);
+                }
+            }
         }
-      }
-    }
-
-    // Iterate over the array once again and using the rows and cols sets, update the elements.
-    for (int i = 0; i < R; i++) {
-      for (int j = 0; j < C; j++) {
-        if (rows.contains(i) || cols.contains(j)) {
-          matrix[i][j] = 0;
+        
+        // System.out.println(r1);
+        // System.out.println(c1);
+        
+        for(int x: r1){
+            for(int i=0;i<col;i++){
+                matrix[x][i]=0;
+            }
         }
-      }
+        
+        for(int y: c1){
+            for(int i=0;i<row;i++){
+                matrix[i][y]=0;
+            }
+        }
+        
     }
-  }
 }
