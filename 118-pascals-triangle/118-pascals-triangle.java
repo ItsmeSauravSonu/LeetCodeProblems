f84d9a87f27fe4c999dbb9e9f94499b52d81f5ad
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-27 02:46:07 +0530

    Time: 1 ms (73.27%), Space: 41.8 MB (51.61%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,26 +1,19 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-    List<List<Integer>> result = new ArrayList<>();
-    if (numRows == 0) return result;
-
-    result.add(new ArrayList<>(Arrays.asList(1))); // seed first row
-
-    for (int i = 1; i < numRows; i++) { // starting from second row because we seeded the first
-      List<Integer> row = new ArrayList<>();
-      row.add(1); // add first 1 in each row
-	  
-      for (int j = 1; j < i; j++) {
-        int left = result.get(i-1).get(j-1);
-        int right = result.get(i-1).get(j);
-        row.add(left + right); 
-      }
-	  
-      row.add(1); // add last 1 to each row
-      result.add(row);
-    }
-
-    return result;
-        
-    
+       List<List<Integer>> ans = new ArrayList<>();
+       List<Integer> row, pre = null;
+        for(int i=0;i<numRows;i++){
+            row = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    row.add(1);
+                }else{
+                    row.add(pre.get(j-1)+pre.get(j));
+                }
+            }
+            pre = row;
+            ans.add(row);
+        }
+        return ans;
     }
 }
