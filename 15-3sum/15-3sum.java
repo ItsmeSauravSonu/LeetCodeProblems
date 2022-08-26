1806ae4ff43cc98a22d200037f41450cc9792aa0
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-27 03:24:06 +0530

    Time: 27 ms (83.11%), Space: 59.4 MB (40.18%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,32 +1,52 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        int size = nums.length;
+        
+        if(size < 3 || nums == null)
+            return result;
         
         Arrays.sort(nums);
         
-        
-       for(int i=0;i<nums.length-2;i++)
-       {
-        int left = i+1;
-        int right = nums.length-1;
-           
-        while(left<right)
+        for(int i=0; i<size-2; i++)
         {
-            int currSum = nums[i]+nums[left]+nums[right];
-            if(currSum==0)
+            if( (i > 0 && nums[i] != nums[i-1]) || i == 0)
             {
-               ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                left++;
-                right--;
-            }else if(currSum<0)
+            int left = i+1;
+            int right = size -1;
+            
+            int sum = 0 - nums[i];
+            
+            while(left < right)
             {
-                left++;
-            }else if(currSum>0)
-            {
-                right--;
+                if(nums[left] + nums[right] == sum)
+                {
+                    List<Integer> sub_ans = new ArrayList<>();
+                    sub_ans.add(nums[i]);
+                    sub_ans.add(nums[left]);
+                    sub_ans.add(nums[right]);
+                    
+                    result.add(sub_ans);
+                    
+                    
+                    while(left < right && nums[left] == nums[left + 1])
+                        left++;
+                    
+                    while(left < right && nums[right] == nums[right - 1])
+                        right--;
+                    
+                    left++;
+                    right--;
+                }
+                else if(nums[left] + nums[right] > sum)
+                    right--;
+                
+                else
+                    left++;
+            }
             }
         }
-       }
-        return new ArrayList(ans);
+        return result; 
     }
 }
