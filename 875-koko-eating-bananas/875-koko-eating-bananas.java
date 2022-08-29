890831bdc223e293aefca3f1f7f55546114f024e
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-29 18:42:04 +0530

    Time: 87 ms (5.10%), Space: 54.4 MB (20.51%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,20 +1,36 @@
 class Solution {
-    public boolean canEat(int[] piles, long mid, int h){
-        long hours=0;
-        for(int pile:piles)
-            hours += (pile/mid)+ (pile%mid == 0 ? 0 : 1);
-        return hours <= h;
+    
+    public boolean valid(int []piles, long mid,int h){
+        long count = 0;
+        for(int i=0;i<piles.length;i++){
+            count += (piles[i]/mid) + (piles[i]%mid==0?0:1);
+        }
+        return count<=h;
+    }
+    
+    public int max(int [] piles){
+        int n = piles.length;
+        long res = Long.MIN_VALUE;
+        for(int x: piles){
+            if(x>res){
+                res = x;
+            }
+        }
+        return (int)res;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        long start =1, end = Arrays.stream(piles).max().getAsInt();
-        long ans =0;
-        while(start <= end){
-            long mid = start +((end-start)/2);
-            if(canEat(piles,mid,h)){
+        long start = 1;
+        long end  = max(piles);
+        long ans = 0;
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            if(valid(piles, mid, h)==true){
                 ans = mid;
                 end = mid-1;
-            }else start = mid+1;
+            }else {
+                start = mid+1;
+            }
         }
         return (int)ans;
-}
+    }
 }
