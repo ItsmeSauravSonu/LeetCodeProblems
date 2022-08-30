949f07d68e5e5fefe9a413b4fcae97456523ad24
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-30 20:27:20 +0530

    Time: 1 ms (99.99%), Space: 53.7 MB (73.75%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,21 +1,23 @@
 class Solution {
-    public void dfs(int[][] M, int[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
+    public void provincesInfo(int [][] isConnected, boolean [] visited, int currentCity){
+        if(visited[currentCity]==false){
+            visited[currentCity]=true;
+            for(int i=0;i<isConnected.length;i++){
+                if(isConnected[currentCity][i]==1)
+                provincesInfo(isConnected , visited , i);
             }
         }
     }
-    public int findCircleNum(int[][] M) {
-        int[] visited = new int[M.length];
-        int count = 0;
-        for (int i = 0; i < M.length; i++) {
-            if (visited[i] == 0) {
-                dfs(M, visited, i);
-                count++;
+    public int findCircleNum(int[][] isConnected) {
+        int numOfCities = isConnected.length;
+        boolean [] visited = new boolean[numOfCities];
+        int countOfProvinces = 0;
+        for(int currentCity=0;currentCity<numOfCities;++currentCity){
+            if(visited[currentCity]==false){
+                countOfProvinces++;
+                provincesInfo(isConnected, visited, currentCity);
             }
         }
-        return count;
-}
+        return countOfProvinces;
+    }
 }
