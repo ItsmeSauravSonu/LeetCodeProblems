6de7993093b8fdbaf3067e9d278a036e52ea31cc
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-08-31 09:52:02 +0530

    Time: 4 ms (93.28%), Space: 47.2 MB (78.22%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,46 +1,43 @@
 class Solution {
-   
-
-    public boolean canFinish(int v, int[][] edge) {
-        //List of List of integer to store the given graph
-					List<List<Integer>> adj = new ArrayList<>();
-					for(int i =0;i<v;i++){
-						adj.add(new ArrayList<>());
-					}
-					for(int i=0;i<edge.length;i++){
-						adj.get(edge[i][0]).add(edge[i][1]);
-					}
-					boolean[] vis = new boolean[v+1];
-					boolean[] stack = new boolean[v+1];
-					for(int i=0;i<v;i++){
-						if(!vis[i]){
-						//if any traversal returns true i.e there is a cycle and no such ordering is possible 
-							if(dfs(adj,vis,stack,i)){
-								return false;
-							}
-						}
-					}
-					return true;
-				}
-				//DFS traversal to find wheter the cycle is present or not in the given graph
-				public boolean dfs(List<List<Integer>> adj , boolean[] vis , boolean[]stack , int key){
-				// mark current vertex as visited and recursively call for its adjacent nodes ....... also mark stack[key] as true to represent the node is    
-				//present in recursive stack
-				
-					vis[key] = true;
-					stack[key] = true;
-					for(int x : adj.get(key)){
-						if(!vis[x]){
-							if(dfs(adj,vis,stack,x)){
-								return true;
-							}
-						}
-						if(stack[x]){
-							return true;
-						}
-					}
-	//	recursive call for current node/ vertex is over so mark stack[key] = false to represent that this node is no longer  in the recursive call stack
-					stack[key] = false;
-					return false;
-				}
-			}
+    public boolean dfs(int node, int [] visited, ArrayList<Integer>[] adj){
+        
+        if(visited[node]==1){
+            return false;
+        }
+        if(visited[node]==2){
+            return true;
+        }
+        
+        visited[node]=1;
+        
+        for(int n: adj[node]){
+            if(!dfs(n, visited, adj)){
+                return false;
+            }
+        }
+        
+        visited[node]=2;
+        return true;
+        
+    }
+    public boolean canFinish(int n, int[][] grid) {
+        
+        ArrayList<Integer> [] adj= new ArrayList[n]; //adjacency list
+        
+        for(int i=0;i<n;i++){
+            adj[i]=new ArrayList<>();
+        }
+        
+        for(int [] pre: grid){
+            adj[pre[0]].add(pre[1]);
+        }
+        
+        int visited [] = new int[n];
+        for(int i=0;i<n;++i){
+            if(!dfs(i, visited, adj)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
