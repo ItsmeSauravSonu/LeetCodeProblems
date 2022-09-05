0ec3d4dfec5ff3c0c39eeae9841471f471894471
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-09-05 11:20:48 +0530

    Time: 25 ms (11.48%), Space: 71.2 MB (18.32%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,35 +1,36 @@
 class Solution {
+    public int solve(int [][] matrix, int i, int j, int n, int m, int [][] moves, int [][]dp){
+        if(i<0 || i>=n || j<0 || j>=m || matrix[i][j]==0) return 0;
+        
+        int res = Integer.MAX_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
+        for(int [] mat: moves){
+            int ni = i+mat[0];
+            int nj = j+mat[1];
+            
+            res = Math.min(res, solve(matrix, ni, nj, n, m, moves,dp)+1);
+            dp[i][j]=res;
+        }
+        return res;
+    }
     public int countSquares(int[][] matrix) {
-         int n=matrix.length;
-        int m=matrix[0].length;
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-              dp[i][0]=matrix[i][0];
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int [][] dp = new int[n+1][m+1];
+        for(int [] d: dp){
+            Arrays.fill(d,-1);
         }
-        for(int i=0;i<m;i++)
-        {
-              dp[0][i]=matrix[0][i];
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if(matrix[i][j]==1)
-                dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
+        int count = 0;
+        int [][] moves = {{1,0},{0,1},{1,1}};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    continue;
+                }else{
+                   count+=solve(matrix, i, j, n, m, moves,dp);  
+                }       
             }
         }
-       
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                sum+=dp[i][j];
-            }
-        }
-        
-        return sum;
-        
+        return count;
     }
 }
