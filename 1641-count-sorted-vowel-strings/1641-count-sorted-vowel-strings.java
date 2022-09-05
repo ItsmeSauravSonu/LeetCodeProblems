a5e881a4de62b39ebf5244dc6af2535f4499d252
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-09-05 10:53:09 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (99.41%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int count(int idx, int n, String vowels){
+    public int count(int idx, int n, String vowels, int [][] dp){
         if(n==0) return 1;
         if(idx>=5) return 0;
+        if(dp[idx][n]!=-1) return dp[idx][n];
+        int pick = count(idx,n-1, vowels,dp);
+        int notPick = count(idx+1, n,vowels,dp);
         
-        int pick = count(idx,n-1, vowels);
-        int notPick = count(idx+1, n,vowels);
-        
-        return pick + notPick;
+        return dp[idx][n]=pick + notPick;
     }
     public int countVowelStrings(int n) {
       String vowels = "aeiou";
-      return count(0,n,vowels);
+      int [][] dp = new int[5][n+1];
+      for(int [] x: dp){
+          Arrays.fill(x,-1);
+      }
+      return count(0,n,vowels,dp);
     }
 }
