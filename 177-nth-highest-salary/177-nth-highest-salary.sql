eaf935df9a6ea576aada06794550af133c5417fa
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2022-09-11 11:36:27 +0530

    Time: 438 ms (51.59%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -4,7 +4,7 @@ BEGIN
   set M = N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT distinct salary from employee order by salary desc
-      limit 1 offset M
+      SELECT distinct salary from Employee order by salary desc
+      limit 1 offset M 
   );
 END
